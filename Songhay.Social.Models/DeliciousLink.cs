8e0082a8bb11e76e9e74b4dc16ff131c7f9a3fd4
Bryan Wilhite <rasx@songhaysystem.com>
2018-02-28 18:07:08 -0800

    :tractor: :sparkles: added Songhay.Social.Models

---
+++ Songhay.Social.Models/DeliciousLink.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Songhay.Social.Models
+{
+    public class DeliciousLink
+    {
+        public Uri Href { get; set; }
+        public DateTime AddDate { get; set; }
+        public bool IsPrivate { get; set; }
+        public string Tags { get; set; }
+        public string Title { get; set; }
+        public string DD { get; set; }
+    }
+}
---
+++ Songhay.Social.Models/TwitterFavorite.cs
@@ -0,0 +1,24 @@
+﻿using LinqToTwitter;
+using Songhay.Extensions;
+using System;
+using System.Linq;
+
+namespace Songhay.Social.Models
+{
+    public class TwitterFavorite : Favorites
+    {
+        public TwitterFavorite(Favorites data, Uri baseUri)
+        {
+            if (data == null) throw new NullReferenceException("The expected Twitter data is not here.");
+
+            var uri = new Uri(data.User.ProfileImageUrl, UriKind.Absolute);
+            var lastSegment = uri.Segments.Last().Split('.').Last().ToLower();
+            var uriBuilder = new UriBuilder(baseUri).WithPath($"{data.User.ScreenNameResponse}.{lastSegment}");
+
+            this.ProfileImageUrl = uriBuilder.Uri.OriginalString;
+            FrameworkTypeUtility.SetProperties(data, this);
+        }
+
+        public string ProfileImageUrl { get; private set; }
+    }
+}
