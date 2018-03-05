8b937018efd708945f45aaec394890af5d118be6
Bryan Wilhite <rasx@songhaysystem.com>
2018-03-05 15:57:24 -0800

    :gem: :arrow_up: NuGet stuff

---
+++ Songhay.Social.Shell.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Songhay.Social.Shell.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
