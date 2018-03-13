e8c752e2967b8620fc6043d2934c3159b5846605
Bryan Wilhite <rasx@songhaysystem.com>
2018-03-12 17:29:39 -0700

    :hammer: :tractor: added extensions for controller

---
+++ Songhay.Social.ModelContext/Extensions/ProgramMetadataExtensions.cs
@@ -0,0 +1,27 @@
+﻿using Songhay.Models;
+using System;
+
+namespace Songhay.Social.ModelContext.Extensions
+{
+    /// <summary>
+    /// Extensions of <see cref="ProgramMetadata"/>
+    /// </summary>
+    public static class ProgramMetadataExtensions
+    {
+        /// <summary>
+        /// Converts <see cref="ProgramMetadata"/>
+        /// to <see cref="RestApiMetadata"/>
+        /// for social twitter.
+        /// </summary>
+        /// <param name="metadata">The metadata.</param>
+        /// <returns></returns>
+        public static RestApiMetadata ToSocialTwitterRestApiMetadata(this ProgramMetadata metadata)
+        {
+            if (metadata == null) throw new ArgumentNullException(nameof(metadata), "The expected Program metadata is not here.");
+
+            var restApiMetadata = metadata.RestApiMetadataSet["SocialTwitter"];
+
+            return restApiMetadata;
+        }
+    }
+}
--- Songhay.Social/Controllers/SocialController.cs
+++ Songhay.Social/Controllers/SocialController.cs
@@ -4,31 +4,44 @@ using Songhay.Models;
 using Songhay.Social.ModelContext;
 using System;
 using System.Collections.Generic;
+using Songhay.Social.ModelContext.Extensions;
 using System.Net;
+using LinqToTwitter;
 
 namespace Songhay.Social.Controllers
 {
+    /// <summary>
+    /// Controls social media API(s)
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.Controller" />
     [Route("api/[controller]")]
     public class SocialController : Controller
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SocialController"/> class.
+        /// </summary>
+        /// <param name="metadata">The metadata.</param>
         public SocialController(ProgramMetadata metadata)
         {
-            this._metadata = metadata;
+            var restApiMetadata = metadata.ToSocialTwitterRestApiMetadata();
+            this.profileImageBaseUri = restApiMetadata.ToTwitterProfileImageRootUri();
+            this.twitterAuthorizer = SocialContext.GetTwitterCredentialsAndAuthorizer(restApiMetadata.ClaimsSet.ToNameValueCollection());
         }
 
+        /// <summary>
+        /// Gets the Twitter favorites.
+        /// </summary>
+        /// <returns></returns>
         [HttpGet]
         [ProducesResponseType(typeof(ICollection<Models.TwitterFavorite>), (int)HttpStatusCode.OK)]
         [Route("twitter-favorites")]
         public IActionResult GetTwitterFavorites()
         {
-            var restApiMetadata = this._metadata.RestApiMetadataSet["SocialTwitter"];
-            var profileImageBaseUri = new Uri(restApiMetadata.ClaimsSet["TwitterProfileImageRoot"], UriKind.Absolute);
-
-            var authorizer = SocialContext.GetTwitterCredentialsAndAuthorizer(restApiMetadata.ClaimsSet.ToNameValueCollection());
-            var favorites = SocialContext.GetTwitterFavorites(authorizer, profileImageBaseUri);
+            var favorites = SocialContext.GetTwitterFavorites(this.twitterAuthorizer, this.profileImageBaseUri);
             return this.Ok(favorites);
         }
 
-        ProgramMetadata _metadata;
+        readonly IAuthorizer twitterAuthorizer;
+        readonly Uri profileImageBaseUri;
     }
 }
