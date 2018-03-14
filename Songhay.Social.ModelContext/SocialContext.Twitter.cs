12727ad5c641817cf8b33fcd8fb9ac867be1a6e7
Bryan Wilhite <rasx@songhaysystem.com>
2018-03-13 19:49:53 -0700

    :microscope: ShouldGetTwitterFavorites() [controller] passes

--- Songhay.Social.ModelContext/SocialContext.Twitter.cs
+++ Songhay.Social.ModelContext/SocialContext.Twitter.cs
@@ -28,24 +28,20 @@ namespace Songhay.Social.ModelContext
             return authorizer;
         }
 
-        public static ICollection<TwitterFavorite> GetTwitterFavorites(IAuthorizer authorizer, Uri profileImageBaseUri)
+        public static IEnumerable<TwitterFavorite> GetTwitterFavorites(IAuthorizer authorizer, Uri profileImageBaseUri)
         {
             var ctx = new TwitterContext(authorizer);
 
-            #region functional members:
-
-            Func<Favorites, bool> isFavoriteType = i => i.Type == FavoritesType.Favorites;
-            Func<Favorites, int, bool> isLimitedTo = (i, count) => i.Count == count;
-            Func<Favorites, bool> isIncludingEntities = (i) => i.IncludeEntities == true;
-
-            #endregion
-
             var data = ctx.Favorites
-                .Where(i => isFavoriteType(i) && isLimitedTo(i, 50) && isIncludingEntities(i))
-                .ToList();
-            if (!data.Any()) throw new TwitterQueryException("No items were found.");
+                .Where(i =>
+                    (i.Type == FavoritesType.Favorites) &&
+                    (i.Count == 50) &&
+                    (i.IncludeEntities == true))
+                .ToArray();
 
-            var favorites = data.Select(i => new TwitterFavorite(i, profileImageBaseUri)).ToList();
+            if ((data == null) || !data.Any()) return Enumerable.Empty<TwitterFavorite>();
+
+            var favorites = data.Select(i => new TwitterFavorite(i, profileImageBaseUri));
             return favorites;
         }
     }
